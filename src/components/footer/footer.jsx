7a1d26404a876887984f7bbf1ba122529394ce74
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-02-23 12:16:23 +0200

    added icons to the project

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,47 +1,62 @@
 import Group1 from '../../images/Group1.jpg';
-
-
-import './footer.css'
+import './footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-columns">
-        <div className="footer-column">
-          <img src={Group1} alt="Logo" />
+    <div>
+      <footer className="footer">
+        <div className="footer-columns">
+          <div className="footer-column">
+            <img src={Group1} alt="Logo" />
+          </div>
+          <div className="footer-column">
+            <h3>Product</h3>
+            <ul className='list-style'>
+              <li>Features</li>
+              <li>Pricing</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Resources</h3>
+            <ul className='list-style'>
+              <li>Blog</li>
+              <li>Webinars</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Company</h3>
+            <ul className='list-style'>
+              <li>About Us</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Subscribe to Our Newsletter</h3>
+            <ul className='list-style'>
+              <li>for product announcements and exclusive insights</li>
+              <li><input placeholder='Input your email'/></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer-column">
-          <h3>Product</h3>
-          <ul className='list-style'>
-            <li>Features</li>
-            <li>Pricing</li>
-          </ul>
+        <hr />
+        <div className="footer-elements">
+          <div className="footer-column">
+            <select name="language">
+              <option value="English">English</option>
+            </select>
+          </div>
+          <div className="footer-column">
+            &#169; Company Name
+          </div>
+          <div className="footer-column">
+            <img src="logo1.jpg" alt="Logo 1" />
+            <img src="logo2.jpg" alt="Logo 2" />
+            <img src="logo3.jpg" alt="Logo 3" />
+          </div>
         </div>
-        <div className="footer-column">
-          <h3>Resources</h3>
-          <ul className='list-style'>
-            <li>Blog</li>
-            <li>Webinars</li>
-          </ul>
-        </div>
-        <div className="footer-column">
-          <h3>Company</h3>
-          <ul className='list-style'>
-            <li>About Us</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-column">
-        <h3>Subscribe to Our Newsletter</h3>
-        <ul className='list-style'>
-            <li>for product announcements and exclusive insights </li>
-            <li> <input/></li>
-          </ul>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
 
 export default Footer;
-
