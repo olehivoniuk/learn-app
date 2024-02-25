99b0402760df31073a93bfbf2c54ff8e8e1e1c11
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-02-25 12:55:44 +0200

    added button JoinUsButton component and styling to it

--- src/components/joinUsBox/joinUsBox.jsx
+++ src/components/joinUsBox/joinUsBox.jsx
@@ -1,3 +1,4 @@
+import JoinUsButton from '../joinUsButton/joinUsButton'
 import './joinUsBox.css'
 const JoinUsBox = () => {
   return (
@@ -5,10 +6,12 @@ const JoinUsBox = () => {
     <div className="dots-container"></div>
     <h1 className='join-us-heading'>Join us</h1>
     <h4>Qui ut exercitation officia proidend enim non tempor tempor ipsum ex nulla ea adipisicing sit consequat enim elit cupidatat o</h4>
+    
     <div className="oval"></div>
     <div className="oval-diagonal"></div>
     <div className="small-oval"></div>
     <div className="dots-container-second"></div>
+    <JoinUsButton />
 </div>
   )
 }
