62c9ebfbf31d21e6415416c1acc0832f935b9a7c
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-03-01 14:36:59 +0200

    added datePicker component, basic layout and styles for the student training page

--- src/components/datePicker/datePickerComponent.jsx
+++ src/components/datePicker/datePickerComponent.jsx
@@ -4,14 +4,10 @@ import { DatePicker } from '@gsebdev/react-simple-datepicker';
 import 'react-responsive-datepicker/dist/index.css';
 
 function DatePickerComponent({ data }) {
-  // Define state for selected date
-  const [selectedDate, setSelectedDate] = useState('01/02/2023');
 
-  // Callback function when user selects a date
+  const [selectedDate, setSelectedDate] = useState(new Date);
   const onChangeCallback = (date) => {
-    // Update state with selected date
     setSelectedDate(date);
-    // You can perform additional actions here if needed
   };
 
   return (
--- src/pages/training/studentTrainings/studentTraining.jsx
+++ src/pages/training/studentTrainings/studentTraining.jsx
@@ -1,15 +1,88 @@
-import React from 'react'
-import DatePickerComponent from '../../../components/datePicker/datePickerComponent'
+import React from 'react';
+import DatePickerComponent from '../../../components/datePicker/datePickerComponent';
+import './studentTraining.css';
 
 const StudentTraining = () => {
   return (
     <div>
+      <div className='content-container'>
+        <div className='date-picker-component'>
+          <div>
+            <h1 style={{ marginTop: '10vh' }}>Trainings</h1>
+          </div>
 
-    <DatePickerComponent />
-    <DatePickerComponent />
-      
+          <div>
+            <div>
+              <button className="btn btn-success mb-3">Add Training</button>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <h2>Search Trainings</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3 mb-3">
+                <label htmlFor="trainerName">Trainer Name:</label>
+                <input type="text" className="form-control mb-4" id="trainerName" placeholder="First Name" />
+                <button className="btn btn-outline-secondary search-button-trainings " type="button" id="button-search">Search</button>
+              </div>
+              <div className="col-md-3 mb-3">
+                <label htmlFor="specialization">Specialization:</label>
+                <input type="text" className="form-control" id="specialization" />
+              </div>
+              <div className="col-md-2"></div>
+              <div className="col-md-2 mb-3">
+                <label htmlFor="fromDate">From:</label>
+                <DatePickerComponent id="fromDate" />
+              </div>
+              <div className="col-md-2 mb-3">
+                <label htmlFor="toDate">To:</label>
+                <DatePickerComponent id="toDate" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div className='table-container'>
+        <h2>My passed trainings </h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>DATE</th>
+              <th>TRAINING NAME 2</th>
+              <th>TYPE</th>
+              <th>TRAINER NAME</th>
+              <th>DURATION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>12.03.23</td>
+              <td>Java Script Course 1</td>
+              <td>Webinar</td>
+              <td>Mattew Martinez</td>
+              <td>15 d</td>
+            </tr>
+            <tr>
+              <td>12.03.23</td>
+              <td>JS Course 2</td>
+              <td>Webinar</td>
+              <td>Mattew Martinez</td>
+              <td>10 d</td>
+            </tr>
+            <tr>
+              <td>12.03.23</td>
+              <td>Java</td>
+              <td>Webinar</td>
+              <td>Maria White </td>
+              <td>2 d</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StudentTraining
+export default StudentTraining;
