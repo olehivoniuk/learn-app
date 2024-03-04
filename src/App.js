1341d508439d848bcf8f15354dc6455acdb3e83d
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-03-04 18:20:57 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Navigation from './components/navigation/navigation';
-import routes from './routs/routes'; // Import your routes
+import routes from './routs/routes';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
