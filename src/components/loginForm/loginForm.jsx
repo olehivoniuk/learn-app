daefe572b21562bab5907fbf9e0c19a4bd7d003e
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-03-08 18:07:23 +0200

    corrected routing

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { addFormData } from '../../redux/userData';
-// import Box from '../box/box'; // Import the Box component
-// import MiniBoxTrainer from '../box/miniBoxTrainer';
 import './loginForm.css';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  // const [submitClicked, setSubmitClicked] = useState(false); // State to track submit button click
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userData = useSelector(state => state.userData);
@@ -27,7 +24,7 @@ const LoginForm = () => {
         // Update the user's isLoggedIn status to true
         dispatch(addFormData({ ...user, isLoggedIn: true }));
         // Redirect the user based on their role
-        navigate(user.role === 'student' ? '/my-account-student' : '/my-account-trainer-profile');
+        navigate(user.role === 'student' ? '/student-login-home' : '/trainer-login-home');
       } else {
         setErrorMessage('Incorrect username or password');
       }
@@ -87,8 +84,6 @@ const LoginForm = () => {
           </label>
         </div>
       </form>
-      {/* {submitClicked && <Box isLoggedIn={false} role={'trainer'} />} Render Box component when submit button is clicked */}
-      {/* {submitClicked && <MiniBoxTrainer isLoggedIn={true} role={'student'} />} */}
     </div>
   );
 };
