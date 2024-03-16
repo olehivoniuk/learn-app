c49f3f9b5b1cadd3a39e702406a1df6bc5fbadbf
Oleh Ivoniuk <olehivoniuk767@gmail.com>
2024-03-16 22:11:22 +0200

    updated box mini-profile component

--- src/components/box/box.jsx
+++ src/components/box/box.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'; // Importing useSelector and useDispatch hooks
+import { useSelector, useDispatch } from 'react-redux';
 import AvatarStudent from '../../images/AvatarStudent.png';
 import AvatarTrainer from '../../images/AvatarTrainer.png';
 import { Link } from 'react-router-dom';
-import { signOut } from '../../redux/userData'; // Import the signOut action
+import { signOut } from '../../redux/userData';
 import './box.css';
 
-const Box = ({ isLoggedIn, userEmail }) => {
+const Box = ({ userEmail }) => {
   const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const userData = useSelector(state => state.userData);
@@ -20,58 +20,37 @@ const Box = ({ isLoggedIn, userEmail }) => {
   const handleSignOutClick = () => {
     dispatch(signOut());
   };
-  
 
-  const renderProfile = () => {
-    
-    if (isLoggedIn) {
-      let profileName, profileImage, accountLink;
-      if (role === 'student') {
-        profileName = userName;
-        profileImage = AvatarStudent;
-        accountLink = '/my-account-student';
-      } else if (role === 'trainer') {
-        profileName = userName;
-        profileImage = AvatarTrainer;
-        accountLink = '/my-account-trainer-profile';
-      }
-  
-      return (
-        <div className="mini-profile ">
-          <div className="profile-icon" onClick={toggleProfile}>
-            <i className="fas fa-user">
-              <div className="profile-info">
-              <div>{profileName}</div>
-              <img src={profileImage} alt="user" style={{ maxWidth: '6vh', borderRadius: "50px"}}  />
-              </div>
-            </i>
-          </div>
-          {isOpen && (
-            <div className="profile-details ">
-              <p className="email">{userEmail}</p>
-              <div className="line"></div>
-              <Link to={accountLink} className="profile-link">
-                My Account
-              </Link>
-              <div className="toggle">
-                <label htmlFor="night-mode-toggle">Night Mode</label>
-                <input type="checkbox" id="night-mode-toggle" />
-              </div>
-              <div className="line"></div>
-              <Link to="/login" className="sign-out-link" onClick={handleSignOutClick}>
-                Sign Out
-              </Link>
-            </div>
-          )}
-        </div>
-      );
-    }
-    return null;
-  };
+  const profileImage = role === "student" ? AvatarStudent : AvatarTrainer;
+  const accountLink = role === "student" ? "/my-account-student" : '/my-account-trainer-profile';
 
   return (
-    <div>
-      {renderProfile()}
+    <div className="mini-profile">
+      <div className="profile-icon" onClick={toggleProfile}>
+        <i className="fas fa-user">
+          <div className="profile-info">
+            <div>{userName}</div>
+            <img src={profileImage} alt="user" style={{ maxWidth: '6vh', borderRadius: "50px"}} />
+          </div>
+        </i>
+      </div>
+      {isOpen && (
+        <div className="profile-details">
+          <p className="email">{userEmail}</p>
+          <div className="line"></div>
+          <Link to={accountLink} className="profile-link">
+            My Account
+          </Link>
+          <div className="toggle">
+            <label htmlFor="night-mode-toggle">Night Mode</label>
+            <input type="checkbox" id="night-mode-toggle" />
+          </div>
+          <div className="line"></div>
+          <Link to="/login" className="sign-out-link" onClick={handleSignOutClick}>
+            Sign Out
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
